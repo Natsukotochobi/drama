e3f0a379339f7e94151037de3bfb34aea03c1ec2
Natsuko <makumaku2014@gmail.com>
2023-06-26 14:21:38 +0900

    enumメソッドをファイル分け

--- src/main/java/com/raisetech/drama/validation/PriorityValidator.java
+++ src/main/java/com/raisetech/drama/validation/PriorityValidator.java
@@ -1,20 +1,18 @@
 package com.raisetech.drama.validation;
 
+import com.raisetech.drama.entity.Priority;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+
 public class PriorityValidator implements ConstraintValidator<PriorityValidation, String> {
-    public enum Letter{
-        //視聴優先度を表す文字を定義する
-        A,B,C
-    }
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         // 自作バリデーションのロジックを実装する
         // 例: valueがA、B、Cのいずれかであるかチェックする
         // バリデーションが成功した場合はtrue、失敗した場合はfalseを返す
-        return Letter.A.name().equals(value) ||
-                Letter.B.name().equals(value) ||
-                Letter.C.name().equals(value);
+        return Priority.A.name().equals(value) ||
+                Priority.B.name().equals(value) ||
+                Priority.C.name().equals(value);
     }
 }
