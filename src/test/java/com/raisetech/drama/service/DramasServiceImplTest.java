94aaafafeda0f89ba7518e10974c649582ab76c6
Natsuko <makumaku2014@gmail.com>
2023-08-01 17:41:05 +0900

    serviceImplクラスのupdateテストを追加

--- src/test/java/com/raisetech/drama/service/DramasServiceImplTest.java
+++ src/test/java/com/raisetech/drama/service/DramasServiceImplTest.java
@@ -3,6 +3,7 @@ package com.raisetech.drama.service;
 import com.raisetech.drama.dto.DramaDto;
 import com.raisetech.drama.entity.Drama;
 import com.raisetech.drama.exception.DuplicateTitleException;
+import com.raisetech.drama.exception.ResourceNotFoundException;
 import com.raisetech.drama.mapper.DramasMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import org.springframework.dao.DuplicateKeyException;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -72,14 +74,15 @@ public class DramasServiceImplTest {
 
     @Test
     void 新しいドラマを登録できるかテストする() {
-        DramaDto dramaDto = new DramaDto("drama1", "2023", "A");
+        DramaDto dramaDto = new DramaDto("Drama1", "2023", "A");
         doNothing().when(dramasMapper).save(dramaDto);
         dramasServiceImpl.save(dramaDto);
         verify(dramasMapper, times(1)).save(dramaDto);
     }
 
     @Test
-    public void DuplicateTitleExceptionがスローされるかテストする() {
+    void saveメソッドでDuplicateTitleExceptionがスローされるかテストする() {
+        //saveメソッドの重複テスト
         DramaDto dramaDto = new DramaDto("Duplicate Title", "2023", "A");
         doThrow(new DuplicateKeyException("重複したタイトル")).when(dramasMapper).save(dramaDto);
         assertThrows(DuplicateTitleException.class, () -> {
@@ -87,6 +90,56 @@ public class DramasServiceImplTest {
         });
         verify(dramasMapper, times(1)).save(dramaDto);
     }
+
+    @Test
+    void 更新が成功した場合をテストする() {
+        DramaDto dramaDto = new DramaDto(1, "Updated Title", "2023", "A");
+        Drama drama = new Drama(1, "Existing Title", "2022", "B");
+        //findById()のモック挙動を定義
+        doReturn(Optional.of(drama)).when(dramasMapper).findById(dramaDto.getId());
+        //update()のモック挙動を定義
+        doNothing().when(dramasMapper).update(drama);
+        // テスト対象メソッドを呼び出す
+        Drama updatedDrama = dramasServiceImpl.update(dramaDto);
+
+        assertThat(updatedDrama.getId()).isEqualTo(dramaDto.getId());
+        assertThat(updatedDrama.getTitle()).isEqualTo(dramaDto.getTitle());
+        assertThat(updatedDrama.getYear()).isEqualTo(dramaDto.getYear());
+        assertThat(updatedDrama.getPriority()).isEqualTo(dramaDto.getPriority());
+
+        verify(dramasMapper, times(1)).findById(dramaDto.getId());
+        verify(dramasMapper, times(1)).update(drama);
+    }
+
+    @Test
+    void updateメソッドで存在しないidが指定された時例外がスローされるかテストする() {
+        //update()のfindById()テスト
+        DramaDto dramaDto = new DramaDto(999, "Updated Title", "2023", "A");
+        doReturn(Optional.empty()).when(dramasMapper).findById(999);
+
+        assertThrows(ResourceNotFoundException.class, () -> dramasServiceImpl.update(dramaDto));
+        verify(dramasMapper, times(1)).findById(999);
+    }
+
+    @Test
+    void updateメソッドでDuplicateTitleExceptionがスローされるかテストする() {
+        DramaDto dramaDto = new DramaDto(1, "Updated Title", "2023", "A");
+        Drama drama = new Drama(1, "Existing Title", "2022", "B");
+        //findById()のモック挙動を定義
+        doReturn(Optional.of(drama)).when(dramasMapper).findById(dramaDto.getId());
+        //update()のモック挙動を定義
+        doThrow(new DuplicateKeyException("重複したタイトル")).when(dramasMapper).update(drama);
+
+        assertThrows(DuplicateTitleException.class, () -> dramasServiceImpl.update(dramaDto));
+
+        verify(dramasMapper, times(1)).findById(dramaDto.getId());
+        verify(dramasMapper, times(1)).update(drama);
+    }
+
+    @Test
+    void メソッドで存在しないidが指定された時例外がスローされるかテストする() {
+
+
 
 
 }
