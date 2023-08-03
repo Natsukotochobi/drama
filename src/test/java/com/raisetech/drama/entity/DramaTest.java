54a921c69b902a30cbab4b76383ef2a68c6d260e
yoshi-koyama <koyama@reytech.co.jp>
2023-08-03 15:28:15 +0900

    Dramaに対するテストクラスを作成

---
+++ src/test/java/com/raisetech/drama/entity/DramaTest.java
@@ -0,0 +1,26 @@
+package com.raisetech.drama.entity;
+
+import com.raisetech.drama.dto.DramaDto;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DramaTest {
+
+    @Test
+    void inputの値が存在するときに更新ができること() {
+        Drama drama = new Drama(1, "title", "year", "priority");
+        DramaDto dramaDto = new DramaDto("title2", "year2", "priority2");
+        drama.updateDrama(dramaDto);
+
+        assertThat(drama).isEqualTo(new Drama(1, "title2", "year2", "priority2"));
+    }
+
+    @Test
+    void inputの値がnullであるときに更新ができること() {
+        Drama drama = new Drama(1, "title", "year", "priority");
+        DramaDto dramaDto = new DramaDto(null, null, null);
+        drama.updateDrama(dramaDto);
+        assertThat(drama).isEqualTo(new Drama(1, "title", "year", "priority"));
+    }
+}
